753a8752036ac3dfab6012b19af42de8ace5ac88
Esteban Perez <esteban.perez7391@hotmail.com>
2018-11-19 14:34:06 -0500

    can now leave review

---
+++ SPARK/review.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    console.log("Hello");
+    document.getElementById("submit").addEventListener("click",function(){
+    document.getElementById("userName").innerHTML=username.value;
+    document.getElementById("review").innerHTML=star.value + " stars " + reply.value;
+    });
+}
