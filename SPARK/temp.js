156dc147b1e9ffb8b16e9a3f800d13f80d9bfb28
Esteban Perez <esteban.perez7391@hotmail.com>
2018-11-18 20:27:56 -0500

    finished weather api

--- SPARK/temp.js
+++ SPARK/temp.js
@@ -1,10 +1,10 @@
-let weatherApiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Tampa,us&units=imperial&APPID=859d47520ac736ebe8bbce1fef74269c';
-function getTampaWeather() {
+window.onload = function(){
+    let weatherApiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Venice,us&units=imperial&APPID=859d47520ac736ebe8bbce1fef74269c';
     fetch(weatherApiUrl).then((response) => {return response.json();
     }).then((data) => {
         tampaWeather = data;
         console.log(data);
         console.log('the temperature is: '+tampaWeather.main.temp+' degrees');
-        document.getElementById('weatherDiv').innerHTML = tampaWeather.main.temp;
+        document.getElementById('temp').innerHTML = 'The weather in San Marco, Venice is: '+tampaWeather.main.temp+' degrees Fahrenheit';
     });
 }
