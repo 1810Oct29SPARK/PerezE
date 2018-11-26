875887f390c69fe468b0af7b74a842b8ad7e6827
Esteban Perez <esteban.perez7391@hotmail.com>
2018-11-26 11:01:12 -0500

    conrol flow

---
+++ Eclipse/HelloWorld/src/com/revature/example/Calculator.java
@@ -0,0 +1,30 @@
+package com.revature.example;
+
+public class Calculator {
+
+		public double add(double arg0, double arg1) {
+			double add= arg0+arg1;
+			return add;
+		}
+
+		public double subtract(double arg0, double arg1) {
+			double sub= arg0-arg1;
+			return sub;
+		}
+
+		public double multiply(double arg0, double arg1) {
+			double mult= arg0*arg1;
+			return mult;
+		}
+
+		public double divide(double arg0, double arg1) {
+			double div= arg0/arg1;
+			return div;
+		}
+
+		@Override
+		public String toString() {
+			return "Calculator []";
+		}
+
+}
