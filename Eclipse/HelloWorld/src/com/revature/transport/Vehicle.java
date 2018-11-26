17354efb0e82c1087b72e6eea5549bf6bfbdf15d
Esteban Perez <esteban.perez7391@hotmail.com>
2018-11-26 15:15:00 -0500

    abstract classes

---
+++ Eclipse/HelloWorld/src/com/revature/transport/Vehicle.java
@@ -0,0 +1,19 @@
+package com.revature.transport;
+
+
+/*
+ * Abstract class cannot be instantiated 
+ * if there is a ONE (or more) abstract method(s), THE CLASS MUST BE SBSTRACT
+ * (note: opposite of 'abstract' in java in 'concrete'(but there is no kw for it))
+ */
+public abstract class Vehicle {
+
+	public Vehicle() {
+		super();
+		System.out.println("vehicle consructor completed");
+	}
+	/*
+	 * I will have to implement this method in the first concrete subclass
+	 */
+	public abstract void move();
+}
