b5995ea9c9bcfb9596ffbe32edbf52171e7e93af
Esteban Perez <esteban.perez7391@hotmail.com>
2018-12-23 16:42:16 -0500

    cant populate reimbursemsnts to approve for managers

--- Project 1/Project1/src/main/java/project1/main/Driver.java
+++ Project 1/Project1/src/main/java/project1/main/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
 		//emp.removeEmployee(14);
 		//System.out.println(log.getLoginInfoById(3));
 		//System.out.println(reim.getReimbursementById(6));
-		reim.updateReimbursement(7, "approved", "Mary");
+		//reim.updateReimbursement(7, "approved", "Mary");
 //		List<Reimbursement> reimbursement = reim.getReimbursement();
 //		for (Reimbursement r : reimbursement) {
 //			System.out.println(r);
